4eab9e2b57f63e8d4433389228f717f63ed97643
Ash Hoskins <s559245@nwmissouri.edu>
2023-01-28 06:58:37 -0600

    module 3

--- user_numeric_lists.py
+++ user_numeric_lists.py
@@ -5,23 +5,76 @@ Modify this docstring.
 
 # import some modules first - how many can you make use of?
 
-import math
+
 import statistics as stats
 import numpy as np
 import random as rn
-from example_numeric_lists import score_list
+import math
+
+
 
 #Creats a numpy array from DSCI Measurments.
 data = np.loadtxt('DSCI.txt', dtype='int')
 
 #Declare and create lists for Task 3.
-list1 = score_list
+list1 = [
+    105,
+    129,
+    87,
+    86,
+    111,
+    111,
+    89,
+    81,
+    108,
+    92,
+    110,
+    100,
+    75,
+    105,
+    103,
+    109,
+    76,
+    119,
+    99,
+    91,
+    103,
+    129,
+    106,
+    101,
+    84,
+    111,
+    74,
+    87,
+    86,
+    103,
+    103,
+    106,
+    86,
+    111,
+    75,
+    87,
+    102,
+    121,
+    111,
+    88,
+    89,
+    101,
+    106,
+    95,
+    103,
+    107,
+    101,
+    81,
+    109,
+    104,
+]
 listx = [*range(10)]
 listy =[]
 listy2 = []
 i = 0
 
-#Convert numpy array from DSCI measurements file to List elements file type
+#Convert numpy array from DSCI measurements file to List elements data type
 for n in data:
     listy.append(n)
 
@@ -31,24 +84,77 @@ while i != len(listx):
     listy2.append(listx[i] + rn.randrange(0,10))
     i+= 1
 
-
-
-
-
-
 # define some functions
 #Task 3. List 1: function that takes all data from a given list 
 #and provides all statistical information.
-def superstats(information):
-    s1 = [stats.mean(information), stats.median(information), stats.mode(information), stats.pstdev(information), stats.variance(information)]
+def superstats(info):
+    s1 = [stats.mean(info), stats.median(info), stats.mode(info), stats.pstdev(info), stats.variance(info)]
     return s1
 
-def corr()
+#Task 3 List 2: Calculates correlation between two lists and 
+#predicts new y value based on future value of 15; returns list with both results.
+def corr(xlist, ylist):
+    fv = 15
+    slope, intercept = stats.linear_regression(xlist, ylist)
+    y = float(slope * fv + intercept)
+    xx = stats.correlation(xlist, ylist)
+    s2 = [xx, y]
+    return s2
+    
+#Task 3 List 3. Utilizing built in python functions on lists. Returns list of results.
+def list3(newlist):
+    min = min(newlist)
+    max = max(newlist)
+    len = len(newlist)
+    sum = sum(newlist)
+    avg = round(sum/len)
+    set = set(newlist)
+    sort1 = sorted(newlist)
+    sort2 = sorted(newlist, reverse= True)
 
+    s3 = [min, max, len, sum, avg, set, sort1, sort2]
+    return s3
 
+#Task 3 List 4. List methods
+def methody(anotherlist1, anotherlist2):
+    
+    lst = anotherlist1.append(700)
+    lst = anotherlist1.extend(anotherlist2)
+    lst = anotherlist1.insert(5, 277)
+    lst = anotherlist1.remove(700)
+    lst = anotherlist1.append(lst.count(93))
+    lst = anotherlist1.sort()
+    lst = anotherlist1.sort(reverse = True)
+    lst.pop(0)
+    
+    v = len(lst)
+    
+    lst.pop(v-1)
+    return lst
+#Task 3. List 5. List Transformations. Filter function that 
+#returns boolean on if the value is less than or equal to 100 
+def filter1(list2):
+    if list2 <= 100:
+        return True
+    else:
+        return False
 
+#Task 3. List 5 Map function utilizes the map method of taking the 
+#cube root of each element in a given list
+def map1(it):
+    
+    zz = list(map(lambda x: np.cbrt(x), it))
+    aa =np.around(zz, decimals=2)
 
+    return aa
 
+#Task 3 List 5 Calculating the Volume of a given list of sides. 
+#Utlizing the Map method.
+def vol(attribute):
+    dz = list(map(lambda x: math.pow(x,3), attribute))
+
+    return dz
+    
 
 def get_area_of_lot(length, width):
     """
@@ -61,7 +167,7 @@ def get_area_of_lot(length, width):
     # Use a try / except / finally block when something
     # could go wrong
     try:
-        area = 0  # fix this
+        area = length * width  # fix this
         return area
     except Exception as ex:
         print(f"Error: {ex}")
@@ -82,7 +188,8 @@ if __name__ == "__main__":
 
     # call your functions here (see instructions)
     statinfolisty = superstats(listy)
-    
+print(list(filter(filter1, list1)))
+print(vol(listx))
 
 
 
