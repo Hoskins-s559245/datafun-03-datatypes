6ece3cdb59213e48bbc0188fd7af8190e5aaf240
Ash Hoskins <s559245@nwmissouri.edu>
2023-01-31 03:09:53 -0600

    Module 3 Final

--- user_string_lists.py
+++ user_string_lists.py
@@ -1,6 +1,11 @@
 """
-Modify this docstring.
+Student Name: Ash Hoskins, Student #S559245
+Course: CSIS 44-609 - Data Analytics Fundamentals
+Professor Denise Case
+Domain: Geospatial data
+Module 3: Assignment 1 Task 4.
 
+Working with String Lists
 """
 
 # imports first
@@ -8,15 +13,17 @@ import random as rn
 
 
 countries = ['Chile', "United States", "Canada", "Mexico", "Columbia"]
-cities = ['Santiago', "Los Angelas", "Toronto", "Tijuana", "Bogota"]
+cities = ['Santiago', "Washington D.C", "Ottowa", "Tijuana", "Bogota"]
 landscape = ['Mountains', 'Plains', 'Artic', 'Desert', "Forest"]
 dsci = ['D1', 'D3', 'D1','D4', 'D0']
 rainfall = ['Less than 3000', 'Less than 2000', 'Less than 3000', 'Less than 1000', 'Greater than 3000']
 
-#with open("DSCI.txt", "r") as fileObject:
-#    text = fileObject.read()
-#    items = text.split() 
-#    values = set(items) 
+
+#String Lists 3. Get Unique Words from file
+with open("DSCI.txt", "r") as fileObject:
+    text = fileObject.read()
+    items = text.split() 
+    values = set(items) 
 
 #String Lists 1. Using Python Built-in Functions 
 list1 = list(zip(countries, cities, landscape, dsci, rainfall))
@@ -28,27 +35,35 @@ def city(name, sets):
     cn, ci, ld, ds, rain = zip(*sets)
     i = 0
 
-    while name != ci[i]:
-        i+=i 
+    while name is not ci[i]:
+        i = i + 1 
     
     if name == ci[i]:
         print(name, " Is The Capital of: ", cn[i] )
         print("The landscape is generally made up of: ", ld[i])
         print("The Drough Severity and Coverage Index is: ", ds[i])
-        print("This DSCI indicated a rain fall of", rain[i], "mm per year")
-
-
-
+        print("This DSCI indicates a rain fall of", rain[i], "mm per year")
+        i = 0
 
 
 
 # call functions and execute code
 #Randomly select a city and pass it to the function
-z = rn.choice(['Santiago', "Los Angelas", "Toronto", "Tijuana", "Bogota"])
+z = rn.choice(['Santiago', "Washington D.C", "Ottowa", "Tijuana", "Bogota"])
 city(z, list1)
 
-#print()
-#print("The list of DSCI values for Santiago are: ", items)
+#Set value of length of values from DSCI file.
+dz = len(values)
+
 
 
 # use if __name__ == "__main__":
+#String Lists 3. Get Unique Words from file
+if __name__ == "__main__":
+
+    print()
+    print("The list of DSCI values for Santiago are: ", items)
+    print("List of Unique DSCI Values for Santiago are: ", values)
+    print("List of Values sorted:", sorted(values))
+    print("The length of the list is: ", dz)
+    
---
+++ user_string_lists_out.py
@@ -0,0 +1,68 @@
+"""
+Student Name: Ash Hoskins, Student #S559245
+Course: CSIS 44-609 - Data Analytics Fundamentals
+Professor Denise Case
+Domain: Geospatial data
+Module 3: Assignment 1 Task 4.
+
+Working with String Lists:
+
+Terminal Output
+
+Ottowa  Is The Capital of:  Canada
+The landscape is generally made up of:  Artic
+The Drough Severity and Coverage Index is:  D1
+This DSCI indicates a rain fall of Less than 3000 mm per year
+
+List of Values sorted: ['117', '131', '155', '158', '64', '69', '72', '93', '96']
+The length of the list is:  9
+PS C:\Users\Hoski\Desktop\NWMS\CSIS 44-608 - Data Analytics Fundamentals\Modules\Module 3\datafun-03-datatypes> & C:/Users/Hoski/miniconda3/python.exe "c:/Users/Hoski/Desktop/NWMS/CSIS 44-608 - Data Analytics Fundamentals/Modules/Module 3/datafun-03-datatypes/user_string_lists.py"
+Bogota  Is The Capital of:  Columbia
+The landscape is generally made up of:  Forest
+The Drough Severity and Coverage Index is:  D0
+This DSCI indicates a rain fall of Greater than 3000 mm per year
+
+List of Values sorted: ['117', '131', '155', '158', '64', '69', '72', '93', '96']
+The length of the list is:  9
+PS C:\Users\Hoski\Desktop\NWMS\CSIS 44-608 - Data Analytics Fundamentals\Modules\Module 3\datafun-03-datatypes> & C:/Users/Hoski/miniconda3/python.exe "c:/Users/Hoski/Desktop/NWMS/CSIS 44-608 - Data Analytics Fundamentals/Modules/Module 3/datafun-03-datatypes/user_string_lists.py"
+Washington D.C  Is The Capital of:  United States
+The landscape is generally made up of:  Plains
+The Drough Severity and Coverage Index is:  D3
+This DSCI indicates a rain fall of Less than 2000 mm per year
+
+The list of DSCI values for Santiago are:  ['72', '69', '64', '96', '93', '117', '131', '131', '158', '155', '155', '158', '158', '155']
+List of Unique DSCI Values for Santiago are:  {'64', '72', '96', '69', '131', '93', '158', '117', '155'}
+List of Values sorted: ['117', '131', '155', '158', '64', '69', '72', '93', '96']
+The length of the list is:  9
+PS C:\Users\Hoski\Desktop\NWMS\CSIS 44-608 - Data Analytics Fundamentals\Modules\Module 3\datafun-03-datatypes> & C:/Users/Hoski/miniconda3/python.exe "c:/Users/Hoski/Desktop/NWMS/CSIS 44-608 - Data Analytics Fundamentals/Modules/Module 3/datafun-03-datatypes/user_string_lists.py"
+Washington D.C  Is The Capital of:  United States
+The landscape is generally made up of:  Plains
+The Drough Severity and Coverage Index is:  D3
+This DSCI indicates a rain fall of Less than 2000 mm per year
+
+The list of DSCI values for Santiago are:  ['72', '69', '64', '96', '93', '117', '131', '131', '158', '155', '155', '158', '158', '155']
+List of Unique DSCI Values for Santiago are:  {'64', '96', '158', '93', '117', '155', '131', '69', '72'}
+List of Values sorted: ['117', '131', '155', '158', '64', '69', '72', '93', '96']
+The length of the list is:  9
+PS C:\Users\Hoski\Desktop\NWMS\CSIS 44-608 - Data Analytics Fundamentals\Modules\Module 3\datafun-03-datatypes> & C:/Users/Hoski/miniconda3/python.exe "c:/Users/Hoski/Desktop/NWMS/CSIS 44-608 - Data Analytics Fundamentals/Modules/Module 3/datafun-03-datatypes/user_string_lists.py"
+Tijuana  Is The Capital of:  Mexico
+The landscape is generally made up of:  Desert
+The Drough Severity and Coverage Index is:  D4
+This DSCI indicates a rain fall of Less than 1000 mm per year
+
+The list of DSCI values for Santiago are:  ['72', '69', '64', '96', '93', '117', '131', '131', '158', '155', '155', '158', '158', '155']
+List of Unique DSCI Values for Santiago are:  {'96', '155', '93', '69', '64', '72', '117', '158', '131'}
+List of Values sorted: ['117', '131', '155', '158', '64', '69', '72', '93', '96']
+The length of the list is:  9
+PS C:\Users\Hoski\Desktop\NWMS\CSIS 44-608 - Data Analytics Fundamentals\Modules\Module 3\datafun-03-datatypes> & C:/Users/Hoski/miniconda3/python.exe "c:/Users/Hoski/Desktop/NWMS/CSIS 44-608 - Data Analytics Fundamentals/Modules/Module 3/datafun-03-datatypes/user_string_lists.py"
+Santiago  Is The Capital of:  Chile
+The landscape is generally made up of:  Mountains
+The Drough Severity and Coverage Index is:  D1
+This DSCI indicates a rain fall of Less than 3000 mm per year
+
+The list of DSCI values for Santiago are:  ['72', '69', '64', '96', '93', '117', '131', '131', '158', '155', '155', '158', '158', '155']
+List of Unique DSCI Values for Santiago are:  {'131', '93', '158', '155', '117', '64', '96', '69', '72'}
+List of Values sorted: ['117', '131', '155', '158', '64', '69', '72', '93', '96']
+The length of the list is:  9
+PS C:\Users\Hoski\Desktop\NWMS\CSIS 44-608 - Data Analytics Fundamentals\Modules\Module 3\datafun-03-datatypes> 
+"""
